6f84d613a78527b1a44adb3efedf609dfca4e638
matkoson <conradsong@gmail.com>
2018-08-13 00:01:43 +0200

    Completed automated sprite generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp"),
 svgSprite = require("gulp-svg-sprite"),
-rename = require("gulp-rename");
+rename = require("gulp-rename"),
+del = require("del");
+
 
 
 var config = {
@@ -16,7 +18,12 @@ var config = {
   }
 };
 
-gulp.task("createSprite", () => gulp.src("./app/assets/images/icons/**/*.svg")
+gulp.task("beginClean", ()=>
+del(["./app/temp/sprites", "./app/assets/images/sprites"])
+);
+
+gulp.task("createSprite", ["beginClean"], () => gulp.src("./app/assets/images/icons/**/*.svg") //TAKES A FOLDER FULL OF SVGs, AND pipeS IT THROUGH svgSprite(), WHICH WILL=>
+//1. COMBINE IT + 2. TAKE COORDS FROM THE COMBINED SPRITE FILE, AND WILL SAVE IT IN template:
 .pipe(svgSprite(config))
 .pipe(gulp.dest("./app/temp/sprites")));
 
@@ -30,4 +37,7 @@ gulp.task("copySpriteCSS",["createSprite"],
     .pipe(rename("_sprite.css"))
     .pipe(gulp.dest("./app/assets/styles/modules")));
 
-gulp.task("icons", ["createSprite", "copySpriteGraphic","copySpriteCSS"]);
+gulp.task("endClean", ["copySpriteGraphic","copySpriteCSS"], ()=>
+del("./app/temp/sprites"));
+
+gulp.task("icons", ["beginClean", "createSprite", "copySpriteGraphic","copySpriteCSS", "endClean"]);
