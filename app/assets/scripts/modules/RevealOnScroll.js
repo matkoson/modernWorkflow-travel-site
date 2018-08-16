f6c282018954dcbdfd5ced0212b09012959a5dc3
matkoson <conradsong@gmail.com>
2018-08-16 03:12:54 +0200

    Added waypoint animations, for features and testimonial section

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,34 @@
+import $ from "jquery";
+import Waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints";
+
+class RevealOnScroll {
+  constructor(classToCatch, offsetSet) {
+    this.classToCatch = classToCatch;
+    this.offsetSet = offsetSet;
+    this.itemsToReveal = $(this.classToCatch);
+    this.hideInitially();
+    this.createWaypoints();
+  }
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+      console.log(this.itemsToReveal);
+    // let currClass =this.itemsToReveal.getAttribute("class");
+    // this.itemsToReveal.setAttribute("class", currClass+" reveal-item");
+  }
+  createWaypoints() {
+    console.log(this.classToCatch, this.offsetSet);
+    let off = this.offsetSet;
+    this.itemsToReveal.each(function() {
+    let currIt = this;
+    new Waypoint({
+      element: currIt,
+      handler: ()=>
+       $(currIt).addClass("reveal-item--is-visible"),
+      offset: off
+    });
+  })
+  }
+
+}
+
+export default RevealOnScroll;
