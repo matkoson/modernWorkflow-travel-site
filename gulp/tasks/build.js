c70e89fb01c298f297be27f0b906b0a404bf2e65
matkoson <conradsong@gmail.com>
2018-08-20 01:22:48 +0200

    Files ready to deploy, new docs folder for github pages

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,13 +1,39 @@
 let gulp = require("gulp"),
 imagemin = require("gulp-imagemin"),
 del = require("del"),
-usemin = require("gulp-usemin");
+usemin = require("gulp-usemin"),
+rev = require("gulp-rev"),
+cssnano = require("gulp-cssnano"),
+uglify = require("gulp-uglify"),
+browserSync = require("browser-sync").create();
 
 
-gulp.task("deleteDistFolder", () => {
-    return del("./dist");
+gulp.task("previewDist", () => {
+  browserSync.init({
+    server: {baseDir: "docs"},
+    notify: false
+  });
+  }
+);
+
+
+gulp.task("deleteDistFolder", ["icons"], () => {
+    return del("./docs");
   }
 )
+
+gulp.task("copyGeneralFiles", ["deleteDistFolder"], ()=> {
+  let pathsToCopy = ["./app/**/*",
+  "!./app/index.html",
+"!./app/assets/images/**",
+"!./app/assets/styles/**",
+"!./app/assets/scripts/**",
+"!./app/assets/temp",
+"!./app/assets/temp/**"];
+
+  return gulp.src(pathsToCopy)
+  .pipe(gulp.dest("./docs"))
+});
 
 gulp.task("optimizeImages", ["deleteDistFolder"], () => {
   return gulp.src(["./app/assets/images/**/*", "!./app/assets/images/icons",
@@ -19,15 +45,23 @@ gulp.task("optimizeImages", ["deleteDistFolder"], () => {
     multipass: true //OPTIMIZING .svg
   }))
 
-  .pipe(gulp.dest("./dist/assets/images"));
+  .pipe(gulp.dest("./docs/assets/images"));
 });
 
-gulp.task("usemin", ["deleteDistFolder"], ()=> {
+
+gulp.task("useminTrigger", ["deleteDistFolder"], ()=> {
+  gulp.start("usemin");
+});
+
+gulp.task("usemin", ["styles", "scripts"], ()=> {
   return gulp.src("./app/index.html")
-  .pipe(usemin())
-  .pipe(gulp.dest("./dist"));
+  .pipe(usemin({
+    css: [()=>{console.log("rev done"); return rev();}, ()=> {return cssnano()}],
+    js:  [()=>{console.log("rev done"); return rev();}, ()=>{return uglify()}]
+  }))
+  .pipe(gulp.dest("./docs"));
 });
 
 
 
-gulp.task("build", ["deleteDistFolder", "optimizeImages", "usemin"]);
+gulp.task("build", ["deleteDistFolder", "copyGeneralFiles" ,"optimizeImages", "useminTrigger"]);
